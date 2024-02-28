f1f44a9ad91643ebae5b310b994eb406f1036d94
Berutotion <eagle163013@gmail.com>
2024-02-28 17:23:35 +0800

    create get method and create method

--- server.js
+++ server.js
@@ -2,22 +2,15 @@ const http = require('http')
 const todoRouter = require('./todoRouter')
 const sendRes = require('./sendRes')
 const {HttpError} = require('./Error')
+const todoList = [];
 
 const serverListener = (req,res) => {
 const url = req.url.split('/')[1]
-const headerConfig = {
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
-   'Content-Type': 'application/json'
- }
-
 try{
     if(req.url === '/'){
         sendRes(res,200,'Welcome')
     }else if(url === 'todo'){
-        res.writeHead(200,headerConfig)
-        todoRouter(req,res)
+        todoRouter(req,res,todoList)
     }else{
         throw new HttpError('Can\'t find page',404)
     }
@@ -25,6 +18,7 @@ try{
     if(e.statusCode !== undefined){
         sendRes(res,e.statusCode,e.message);
     }else{
+        console.log(e.message)
         sendRes(res,500,'It have some error, please try again .');
     }
     }
--- todoRouter.js
+++ todoRouter.js
@@ -1,18 +1,16 @@
 
+const { HttpError } = require('./Error')
+const sendRes = require('./sendRes')
+const { v4 }  = require('uuid')
 
-const todoRouter = (req,res) => {
-    let data = ''
+
+
+const todoRouter = async(req,res,todoList) => {
     const method = req.method
-    req.on('data' , (chuck) => {
-        data += chuck
-    })
-
-    req.on('end' , () => {
-        data = JSON.parse(data)
-    })
+try{
     switch(method){
         case'GET': 
-        console.log('get method')
+        getAll(res,todoList)
             break;
         case 'DELETE':
         console.log('delete method')
@@ -21,12 +19,57 @@ const todoRouter = (req,res) => {
         console.log('patch method')
             break;
         case 'POST':
-        console.log('post method')
+            const data = await getDataFromRequest(req)
+            createTodo(res,todoList,data)
             break;
         default:
     }
-    
-    res.end()
+}catch(e){
+    if(e.statusCode !== undefined){
+        sendRes(res,e.statusCode,e.message);
+    }else{
+        console.log(e)
+        sendRes(res,500,'It have some error, please try again .');
+    }
+    } 
 }
+
+const getAll = (res,todoList) => {
+    console.log("GET", todoList)
+    sendRes(res,200,todoList)
+}
+
+const createTodo = (res,todoList,data) => {
+    if(!data.title) throw new HttpError('Input does not conform to the format',400)
+    const todo = {
+        title:data.title,
+        id : v4()
+    }
+    todoList.push(todo)
+    sendRes(res,200,todo)
+}
+
+const getDataFromRequest = (req) => {
+    return new Promise((resolve, reject) => {
+        let data = '';
+        
+        req.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        req.on('end', () => {
+            try {
+                const parsedData = JSON.parse(data);
+                resolve(parsedData);
+            } catch (error) {
+                reject(error);
+            }
+        });
+
+        req.on('error', (error) => {
+            reject(error);
+        });
+    });
+};
 
 module.exports = todoRouter;
